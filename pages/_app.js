3d15fec4cf4123afa4699799150b1581226933ae
Chaitanya Kashyap <ckashyap500@gmail.com>
2022-05-21 00:48:29 +0530

    Title update

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
+import Head from "next/head"
 
 export default function App({
   Component, pageProps: { session, ...pageProps }
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps}/>
-    </SessionProvider>
+      <>
+      <Head>
+        <title>Black Pearl</title>
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+      </>
   )
 }
