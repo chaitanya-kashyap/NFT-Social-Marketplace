75d00c3fe318d4bccea7012f9876284782a7384c
Chaitanya Kashyap <ckashyap500@gmail.com>
2022-05-22 21:13:38 +0530

    Footer updated

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Content from "../components/landing/content";
+import Footer from "../components/landing/footer";
 import Navbar from "../components/landing/navbar";
 
 export default function About () {
@@ -6,6 +7,7 @@ export default function About () {
         <>
             <Navbar />
             <Content /> 
+            <Footer />
         </>
     )
 }
--- pages/community.js
+++ pages/community.js
@@ -1,9 +1,11 @@
+import Footer from "../components/landing/footer";
 import Navbar from "../components/landing/navbar";
 
 export default function Community() {
     return (
         <>
             <Navbar />
+            <Footer />
         </>
     )
 }
